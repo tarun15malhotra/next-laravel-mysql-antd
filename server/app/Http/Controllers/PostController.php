7e926001f3ad189072dc65e5edffad6db4891b03
Tarun <tarun15malhotra@gmail.com>
2023-10-25 22:49:32 +0530

    first commit

---
+++ server/app/Http/Controllers/PostController.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Routing\Controller as BaseController;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+class PostController extends BaseController
+{
+    public function index(): JsonResponse
+    {
+        $posts = Post::all()->toArray();
+        return response()->json($posts);
+    }
+    public function create()
+    {
+    }
+    public function store(Request $request) : JsonResponse
+    {
+        $validated = $request->validate([
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+        $post = Post::create($validated);
+        return response()->json(['status' => 'Post created.']);
+    }
+    public function show(Post $post)
+    {
+    }
+    public function edit(Post $post) : JsonResponse
+    {
+        $record = Post::find($post->id)->toArray();
+        return response()->json($record);
+    }
+    public function update(Request $request, Post $post) : JsonResponse
+    {
+        $validated = $request->validate([
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+        $post->update($validated);
+        return response()->json(['status' => 'Post updated.']);
+    }
+    public function destroy(Post $post) : JsonResponse
+    {
+        $post->delete();
+        return response()->json(['status' => 'Post deleted.']);
+    }
+}
